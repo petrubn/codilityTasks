50bdeb87be6bb89be31e96025e77f52c7365fd85
petrubn <buruian.petru@ceiti.md>
2022-04-07 13:16:05 +0300

    changed Queue to list and added Second Solution

--- OddOccurencesInArray/Program.cs
+++ OddOccurencesInArray/Program.cs
@@ -13,52 +13,55 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Program res = new Program();
-            Console.WriteLine(res.solution(0));
-        }
-
-        public int solution(int N)
-        {
+            
             int[] arr =
             {
                 9, 3, 9,
                 3, 9, 7, 
                 9, 11, 11,13
             };
-
-
-            // var res = arr.Distinct();  1 --
             
+            Console.WriteLine("First solution - " + res.solution(arr) + "\n");
+            Console.WriteLine("Second solution - " + res.secondSolution(arr));
+            
+        }
+
+        public int solution(int[] arr)
+        {
             // int occurence = arr.Aggregate((a, b) => a ^ b);  2 + doar 1 
             // int[] group = Array.FindAll(arr, arr => arr.Equals(arr+1));  3, aproape, tot ca 2
-
-
-            /* same */
-
-            Queue<int> occurs = new Queue<int>();
+            
+            var occurs = new List<int> { };
 
             for (int i = 0; i < arr.Length; i++) 
             {
                 int j;
                 for (j = 0; j < arr.Length; j++)
                 {
-                    if (i != j && arr[i] == arr[j])
+                    if (i != j && arr[i] == arr[j]) 
                         break;
                 }
-
                 if (j == arr.Length)
                 {
-                    occurs.Enqueue(j);
+                    occurs.Append(j);
                     return arr[i];
                 }
             }
-
             foreach ( var o in occurs ) Console.WriteLine("oo " + o);
-
-
+            
             Console.WriteLine('\n');
             return 0;
         }
 
-
+        public int secondSolution(int[] arr)
+        {
+            int result = 0;
+            for (int i = 0; i < arr.Length-1; i++)
+            {
+                result ^= arr[i];
+            }
+            return result;
+        }
+        
     }
 }
